57e5cc167bb9c5c60014071891310d9cb8c177c0
Aqua Marine <aquamarinewater2023@gmail.com>
2025-09-03 12:05:18 +0530

    Creating personal portfolio with Reactjs and Threejs and many other libraries. Created until Work Experience

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas, Feedbacks} from './components'
+const App = () => {
+  return (
+    <div className='relative z-0 bg-primary'>
+      <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+        <Navbar />
+        <Hero />
+      </div>
+      <About/>
+      <Experience/>
+      <Tech/>
+      <Works/>
+      <Feedbacks/>
+      <div className='relative z-0'>
+        <Contact/>
+        <StarsCanvas/>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
